c879e15f652d65251b094446aa56e0b7082cd6b4
Siddharth <siddharthsampath98@gmail.com>
2020-08-10 17:52:22 +0530

    Modified app.run for heroku

--- app.py
+++ app.py
@@ -218,5 +218,5 @@ APP = create_app()
 
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
-    # APP.run()
+    # APP.run(host='0.0.0.0', port=8080, debug=True)
+    APP.run()
